bed55c5383f2c12041a55fc2e288a7137d1eceaa
DESKTOP-CT547OD\southkaito <doragonnanase@gmail.com>
2019-03-13 19:52:36 +0900

    追加データ

---
+++ CampChallenge_java/016.データ操作/HTML/SessionServlettest.java
@@ -0,0 +1,122 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package HTML;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author southkaito
+ */
+@WebServlet(name = "SessionServlettest", urlPatterns = {"/SessionServlettest"})
+public class SessionServlettest extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            
+           request.setCharacterEncoding("UTF-8");
+            
+           String name = request.getParameter("txtName");
+           String gendar = request.getParameter("gendar");
+           String hobby = request.getParameter("mulText");
+           
+           
+            
+            
+            HttpSession hs = request.getSession();
+            
+            hs.setAttribute("Name", name);
+            hs.setAttribute("Gendar", gendar);
+            hs.setAttribute("Hobby", hobby);
+            
+            String theName = (String)hs.getAttribute("Name");
+            String theGendar = (String)hs.getAttribute("Gendar");
+            String theHobby = (String)hs.getAttribute("Hobby");
+            
+            out.print(theName);
+            out.print("<br>");
+            out.print(theGendar);
+            out.print("<br>");
+            out.print(theHobby);
+            
+            
+            
+            
+            
+            
+            
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet SessionServlettest</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet SessionServlettest at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
